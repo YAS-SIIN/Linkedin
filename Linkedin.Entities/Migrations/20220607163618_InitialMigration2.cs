6c73a55b637fca9de1bf3046730be6fbac4b6a41
Yasin <yasinasadnezhad@gmail.com>
2022-06-07 21:07:08 +0430

    Add migration

---
+++ Linkedin.Entities/Migrations/20220607163618_InitialMigration2.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Linkedin.Entities.Migrations
+{
+    public partial class InitialMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Users_UserId1",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_UserId1",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_Users_UserId1",
+                table: "Schedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visits_Users_UserId1",
+                table: "Visits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Visits_UserId1",
+                table: "Visits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_UserId1",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_UserId1",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_UserId1",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Visits");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Activities");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "ExternalUserId");
+
+            migrationBuilder.RenameColumn(
+                name: "NextVisitStartDateTime",
+                table: "Schedules",
+                newName: "UpdateDateTime");
+
+            migrationBuilder.RenameColumn(
+                name: "LikeDateTime",
+                table: "Activities",
+                newName: "UpdateDateTime");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Visits",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDateTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Schedules",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Requests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDateTime",
+                table: "Requests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Activities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_UserId",
+                table: "Visits",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_UserId",
+                table: "Schedules",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_UserId",
+                table: "Activities",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Users_UserId",
+                table: "Activities",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_Users_UserId",
+                table: "Schedules",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visits_Users_UserId",
+                table: "Visits",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Users_UserId",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_Users_UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visits_Users_UserId",
+                table: "Visits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Visits_UserId",
+                table: "Visits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_UserId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDateTime",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDateTime",
+                table: "Requests");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalUserId",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdateDateTime",
+                table: "Schedules",
+                newName: "NextVisitStartDateTime");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdateDateTime",
+                table: "Activities",
+                newName: "LikeDateTime");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Visits",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Visits",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Schedules",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Schedules",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Schedules",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Requests",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Requests",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Activities",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Activities",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_UserId1",
+                table: "Visits",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_UserId1",
+                table: "Schedules",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_UserId1",
+                table: "Requests",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_UserId1",
+                table: "Activities",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Users_UserId1",
+                table: "Activities",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_UserId1",
+                table: "Requests",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_Users_UserId1",
+                table: "Schedules",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visits_Users_UserId1",
+                table: "Visits",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
